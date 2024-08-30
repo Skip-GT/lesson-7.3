98804f044c3a8fa4c8dfd98dd92b0605f920c61d
Skip-GT <yaroslav.vasilev.2015@gmail.com>
2024-08-30 14:52:40 +0300

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,44 @@
+import string
+
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            try:
+                with open(file_name, 'r', encoding='utf-8') as file:
+                    words = []
+                    for line in file:
+                        line = line.lower()
+                        line = line.translate(str.maketrans('', '', string.punctuation.replace('-', '')))
+                        words.extend(line.split())
+                    all_words[file_name] = words
+            except Exception as e:
+                print(f"Ошибка при обработке файла {file_name}: {e}")
+        return all_words
+
+    def find(self, word):
+        all_words = self.get_all_words()
+        result = {}
+        for file_name, words in all_words.items():
+            if word in words:
+                result[file_name] = words.index(word)
+        return result
+
+    def count(self, word):
+        all_words = self.get_all_words()
+        result = {}
+        for file_name, words in all_words.items():
+            count = words.count(word)
+            if count > 0:
+                result[file_name] = count
+        return result
+
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
